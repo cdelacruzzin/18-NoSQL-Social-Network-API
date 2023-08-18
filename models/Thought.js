ff23c580ae4e12ab23c4bf0d8b68e6f12791bde4
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-08-18 19:35:10 -0400

    created a thought model with a virtual

---
+++ models/Thought.js
@@ -0,0 +1,36 @@
+const {Schema, model} = require('mongoose');
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            //Use a getter method to format the timestamp on query
+        },
+        username:{
+            type: String,
+            required: true,
+        },
+        reactions: [Reactions],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+//Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+
+ThoughtSchema.virtuals('reactionCount')
+.get(function(){
+return this.reactions.length;
+});
+
--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
 const User = require('./User');
 const thoughts = require('./Thought');
 module.exports = {User, thoughts};
+
+//model index to require Users and Thoughts
