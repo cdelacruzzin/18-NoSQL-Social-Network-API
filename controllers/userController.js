ea7409246a5a6ee84a4a180e572465ed39f8842c
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-08-19 09:28:31 -0400

    installed validator

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ module.exports = {
 
     async getUsers(req, res){
         try {
+            res.json('ho')
             const users = await User.find();
         } catch (error) {
             res.status(500).json(error);
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoures');
-const thoughtRoutes = require('./thoughtsRoutes');
+// const thoughtRoutes = require('./thoughtsRoutes');
 
 router.use('/user', userRoutes);
-router.use('/thoughts', thoughtRoutes);
+// router.use('/thoughts', thoughtRoutes);
 module.exports = router;
