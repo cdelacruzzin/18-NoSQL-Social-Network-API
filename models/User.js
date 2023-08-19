5cc640fd1e70e8b08e3f0b35601e4a02aa118a52
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-08-18 20:01:07 -0400

    creeated a user model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const {Schema, model} = require('mongoose');
+const { isEmail } = require('validator'); //imports email validator
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: [isEmail, 'Not a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            }
+        ]
+    },
+);
+
+
+
+const User = model('user', userSchema);
